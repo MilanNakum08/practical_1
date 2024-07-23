d09dfdc0a6ba439bfb2f0efd4a2c53bfcdd9654e
Milan08 <milan.nakum0802@gmail.com>
2024-07-23 11:55:07 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf( "I am Milan,I take addmision in Nirma University, which is located in ahmdabad, i am in CSe\n");
+    return 0;
+}
---
+++ newfile.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+// this is my ne file
+int main()
+{
+    int t,count=0,p=0,x=0;
+    scanf("%d",&t);
+    for(int i=0; i<t; i++)
+    {
+        int s,r;
+        scanf("%d %d",&s, &r);
+        int a[s],z=r;
+
+        for(int j=0; j<s; j++)
+        {
+            scanf("%d",&a[j]);
+        }
+        while(x<r)
+        {
+            for(int j=0; j<s; j++)
+            {
+                if(a[j]<=r)
+                    count++;
+                    r=r-a[j];
+            }
+            for(int h=0;h<s;h++)
+                a[h]=a[h]+1;
+            for(int t=0; t<s; t++)
+            {
+                if(a[t]>r)
+                    p++;
+                if(p==sizeof(a[s]))
+                    x=a[t];
+            }
+            r=z;
+        }
+        printf("%d",count);
+        count=0;
+        x=0;
+
+    }
+}
